ccf65e744c112850476d65208d38a5a000c72473
Your NameRebakum <anamikaeasmin@hotmail.com>
2025-04-10 20:18:21 +0600

    project init

--- src/app.tsx
+++
@@ -1,9 +0,0 @@
-export function App() {
-  return (
-    <>
-      <div>
-        <h3 className="text-6xl text-center my-5  font-bold">welcome redux</h3>
-      </div>
-    </>
-  );
-}
--- src/home/navber.tsx
+++
@@ -1,7 +0,0 @@
-export default function navber() {
-  return (
-    <div>
-      <h3>i am navber</h3>
-    </div>
-  );
-}
