5069270340db44dc63f09be0351f78981ea4f7a4
Ahsan Tonmoy <ahosant82@gmail.com>
2025-04-12 17:32:28 +0600

    more then ui debug

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -55,7 +55,7 @@ export default function ContactFormPreview() {
   }
 
   return (
-    <div className=" text-black py-20 px-4">
+    <div className=" text-black md:py-20">
       <div className="max-w-6xl mx-auto">
         <h2 className="text-4xl font-bold text-center mb-4 uppercase">
           contact with us
